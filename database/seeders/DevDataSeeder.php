9812304bbbbacdd9446df0b17851a327c4bb707b
Gino Burdea <burdea.gino@gmail.com>
2023-12-15 11:20:37 +0200

    ci: add seeder for development

---
+++ database/seeders/DevDataSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', '=', 'email@example.com')->first();
+        if ($user) {
+            return;
+        }
+        $user = User::factory()->create([
+            'email' => 'email@example.com',
+            'password' => 'email@example.com'
+        ]);
+
+        $state = array_fill(0, 5, ['user_id' => $user->id]);
+        Product::factory()->count(5)->state(...$state)->create();
+        Client::factory()->count(5)->state(...$state)->create();
+    }
+}
